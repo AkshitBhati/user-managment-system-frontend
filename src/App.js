fca7941a3a463b04809e7563c8c0dd3c1ea70474
Akshit Bhati <akshitworks24@gmail.com>
2023-07-13 13:48:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { useSelector } from 'react-redux'
+import { Toaster } from 'react-hot-toast'
 import "./global.css"
 import Home from './Components/Home'
 import Veiw from './Components/Veiw'
@@ -10,6 +10,19 @@ import Navbar from './Components/Navbar'
 const App = () => {
   
   return (
+    <>
+    <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 2000,
+          style: {
+            fontSize: "1.2rem",
+            fontWeight: "bold",
+            background: "rgba(0, 0, 0, 0.8)",
+            color: "#fff",
+          },
+        }}
+      />
     <Router>
       <Navbar />
       <Routes>
@@ -18,6 +31,7 @@ const App = () => {
         <Route path='/profile' element={<Profile   />} />
       </Routes>
     </Router>
+    </>
   )
 }
 
